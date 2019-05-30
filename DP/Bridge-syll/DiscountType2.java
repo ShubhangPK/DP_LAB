1784cdd7647844859094dc22aa531419be7880d6
Shubhang Kaushik <33983863+ShubhangPK@users.noreply.github.com>
2019-05-30 21:41:57 +0530

    Update DiscountType2.java

--- DP/Bridge-syll/DiscountType2.java
+++ DP/Bridge-syll/DiscountType2.java
@@ -2,16 +2,14 @@ package Bridge;
 
 public class DiscountType2 implements iDiscountCalculator {
 	
-	//This class is another concrete implementation of Bridge Pattern - another discount scheme, Type2
-	//It implements the algorithm / logic of "calculateBillAmount" method in "iDiscountCalculator" 
-	//This class is concerned about the type of discount to be applied. In this case 20% discount on MRP and Rs.250 off for all purchases above Rs.6000. 
+	
 
 	public void calculateBillAmount(double mrptotal) {
 		double billamount; double discount; int offamount = 0; 
 		
-		discount = 0.20; //20% discount on MRP
+		discount = 0.20;
 		    
-		    //Give Rs.250 off for all purchases above Rs.6000. 
+		    
 			if(mrptotal > 6000) offamount = 250; 
 			billamount = mrptotal - discount * mrptotal - offamount;
 			
