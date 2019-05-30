5bfef7f27e370c48fbc337f3f9eafe02e86c17e1
Shubhang Kaushik <33983863+ShubhangPK@users.noreply.github.com>
2019-05-30 21:39:01 +0530

    Update BridgeClientMain.java

--- DP/Bridge-syll/BridgeClientMain.java
+++ DP/Bridge-syll/BridgeClientMain.java
@@ -6,23 +6,20 @@ public class BridgeClientMain {
 		
     double mrptotal = 8000.50; 
 		
-        RegCust oRC = new RegCust("Sindhu", 24); 
-		SeniorCitizenCust oSCC = new SeniorCitizenCust("Vijay Amrithraj",62); 
+        RegCust oRC = new RegCust(" X ", 24); 
+		SeniorCitizenCust oSCC = new SeniorCitizenCust(" Y ",62); 
 		
-		//Here, the DiscountType can be varied between customers - meaning the discount type is not tied to a customer
-		//setDiscountType method lets the cutomer's discount type to be varied 
 		
 		DiscountType1 odtype1 = new DiscountType1(); 
 		oRC.display(); 		
 		DiscountType2 odtype2 = new DiscountType2(); 
-		oRC.setDiscountType(odtype2); //Regular Customer is given discount type 2 
-		 //oDC.setDiscountType(odtype1); //Regular Customer can be given discount type 1 if uncommented  
+		oRC.setDiscountType(odtype2); 
 		
 		oRC.calculateDiscount(mrptotal);	
 		
 		
 		oSCC.display(); 
-		oSCC.setDiscountType(odtype1); //Regular Customer is given discount type 2 
+		oSCC.setDiscountType(odtype1); 
 		oSCC.calculateDiscount(mrptotal);
 		 
 	}
