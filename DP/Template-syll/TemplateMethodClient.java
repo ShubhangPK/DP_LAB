bb1bea93ddf9ca8ef1356314ba427ff5812b729b
Shubhang Kaushik <33983863+ShubhangPK@users.noreply.github.com>
2019-05-30 20:46:14 +0530

    Update TemplateMethodClient.java

--- DP/Template-syll/TemplateMethodClient.java
+++ DP/Template-syll/TemplateMethodClient.java
@@ -4,10 +4,6 @@ public class TemplateMethodClient {
 
 	public static void main(String[] args) {
 		OnlineOrderProcessing oOnline = new OnlineOrderProcessing(); 
-		//Here "processOrder" is the Template Method Pattern 
-		/* It consists of sub-methods 
-		 *  selectItem(); doPayment(); doDelivery();
-		 */
 		oOnline.processOrder(); 
 	}
 
